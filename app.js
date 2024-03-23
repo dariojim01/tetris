ea1d5fe52cd5d17cc129ada64b9de4c007760cac
Dario Jimenez <dariojim01@gmail.com>
2024-03-23 12:38:01 -0500

    app.js obteniendo elementos en constantes

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
+    const grid=document.querySelector('.grid');
+    let squares= Array.from(document.querySelectorAll('.grid div'));
+    const ScoreDisplay = document.querySelector('#score');
+    const StartButton = document.querySelector('#start-button');
 
+
+
+
+
+    nst width = 10;
+    const heigth = 20;
+    console.log(squares);
 });
